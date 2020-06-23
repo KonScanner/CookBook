d99f1346d9090610bf4eb372d91d4c9d83176c37
Konstantinos Kompogiannopoulos <42744293+KonScanner@users.noreply.github.com>
2020-06-23 22:52:46 +0100

    init_examples_1

---
+++ Python/native_modules/Interactive_Example/README.md
@@ -0,0 +1,12 @@
+# File name changer/reformater for data files.
+
+- Uses native modules:
+
+  - `re` : regex
+  - `sys` : system
+  - `os` : os
+  - `calendar` : calendar
+
+- For this example to work the filename is expected to be in the form `data-DD-MMM-YY.txt`
+
+- To run provide it with a path to the data directory, else it will assume that you are running the file in a folder that contains a `data` dir.
---
+++ Python/native_modules/Interactive_Example/main.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import calendar
+import re
+
+months = [i.lower() for _, i in enumerate(calendar.month_abbr)][1:]
+
+
+def name_changer(directory_name: str = dir_name):
+    """Filename is expected to be in the form 'data-DD-MMM-YY.txt'."""
+    for filename in os.listdir(dir_name):
+        regex = r"([A-Za-z0-9]+)\w+"  # to match file details
+        if [match.group(0) for matchNum, match in enumerate(
+                re.finditer(r"(.txt)", filename, re.MULTILINE), start=1)][0] in filename:
+
+            match = [match.group(0) for matchNum, match in enumerate(
+                re.finditer(regex, filename, re.MULTILINE), start=1)]
+
+            d, month, y = int(match[1]), match[2], int(match[3])
+            m = months.index(month.lower()) + 1
+
+            newname = '{}-20{:02d}-{:02d}-{:02d}.txt'.format(match[0], y, m, d)
+
+            newpath = os.path.join(dir_name, newname)
+            oldpath = os.path.join(dir_name, filename)
+
+            print(f'{oldpath}->{newpath}')
+            os.rename(oldpath, newpath)
+
+
+if __name__ == "__main__":
+    dir_name = str(sys.argv[1])
+    if dir_name == None:
+        print('Assumed dir_name == \'data\':')
+        name_changer(directory_name=str(os.getcwd()) + r'\data')
+    else:
+        print(f'Using directory path {dir_name}')
+        name_changer(directory_name=dir_name)
