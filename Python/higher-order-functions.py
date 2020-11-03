6a39377741ef8682f065a7e516f153d7d4174c2a
Konstantinos Kompogiannopoulos <42744293+KonScanner@users.noreply.github.com>
2020-11-03 23:29:38 +0000

    higher order function example 1 with tests

---
+++ Python/higher-order-functions.py
@@ -0,0 +1,49 @@
+"""You can reduce duplication
+by using higher order functions.
+
+For example, turning add_two(), into
+
+add_n()
+
+See bellow:
+"""
+
+
+def add_two(x: int) -> int:
+    if isinstance(x, int):
+        return x + 2
+    else:
+        eval("help(add_two)")
+        raise TypeError("Use integers!")
+
+
+def _test_add_two():
+    assert add_two(-2) == 0
+    assert add_two(0) == 2
+    assert add_two(2) == 4
+
+
+_test_add_two()
+# A higher order function that would add n to x would be:
+
+
+def add_n(n: int) -> int:
+    """add_x(x: int) -> int
+    """
+    def add_x(x: int) -> int:
+        if isinstance(x, int) and isinstance(n, int):
+            return x+n
+        else:
+            eval("help(add_n)")
+            raise TypeError("Use integers!")
+    return add_x
+
+
+def _test_add_n():
+    assert add_n(50)(30) == 80
+    assert add_n(30)(-30) == 0
+    assert add_n(-50)(-30) == -80
+
+
+_test_add_n()
+add_n(20)(1.5)
